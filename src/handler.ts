7a116ec4650d7569b2cf28ebea74a70f2a91a78d
Rholang <rholang@protonmail.com>
2021-07-06 22:08:59 +0200

    up

--- src/handler.ts
+++ src/handler.ts
@@ -90,33 +90,41 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
         }, 0)
       /* Cache matching */
       const { hostname } = new URL(request.url)
-      const concatUrl = 'https://' + hostname + '/' + hashCode(code).toString()
+      const concatUrl = 'https://' + hostname + '/' + code
 
-      const cacheUrl = new URL(concatUrl)
+      const cacheUrl = new URL(request.url)
       //const cacheUrl = new URL('httpcode')
       const cacheKey = new Request(cacheUrl.toString(), request)
       const cache = caches.default
 
       const response = await cache.match(cacheKey)
+      console.log(response)
 
       if (!response) {
-        const { result } = await exploreRequest(net, code)
+        //const { result } = await exploreRequest(net, code)
         // Must use Response constructor to inherit all of response's fields
-        const response = new Response(JSON.stringify(result), { status: 200 })
+        const response = new Response(JSON.stringify('notcachedresponse'), {
+          status: 200,
+        })
 
         // Cache API respects Cache-Control headers. Setting s-max-age to 10
         // will limit the response to be in cache for 10 seconds max
 
         // Any changes made to the response here will be reflected in the cached value
-        response.headers.append('Cache-Control', 's-maxage=31556952')
+        response.headers.append('Cache-Control', 'public maxage=31556952')
 
         // Store the fetched response as cacheKey
         // Use waitUntil so you can return the response without blocking on
         // writing to cache
         event.waitUntil(cache.put(cacheKey, response.clone()))
         return response
+      } else {
+        const response = new Response(JSON.stringify('cachedresponse'), {
+          status: 200,
+        })
+        response.headers.append('Cache-Control', 'public maxage=31556952')
+        return response
       }
-      return response
     }
     const response = new Response('Expected Arguments', { status: 500 })
     return response
