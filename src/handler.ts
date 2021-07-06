e9881348bb7e860fc5fcc142db1673375fae6d71
Rholang <rholang@protonmail.com>
2021-07-06 21:11:21 +0200

    get request

--- src/handler.ts
+++ src/handler.ts
@@ -65,60 +65,61 @@ const readRequestBody = async (request: Request) => {
   }
 }
 
-const exploreRequest = async (request: Request) => {
-  // Construct the cache key from the cache URL
-  const { net, code } = await readRequestBody(request)
-  console.log(net)
+const exploreRequest = async (net: string, code: string) => {
+  const node = getNode(net)
+  const { exploreDeploy } = createRnodeService(node)
+  const result = await exploreDeploy({
+    code: code,
+  })
 
-  if (net && code) {
-    const node = getNode(net)
-    const { exploreDeploy } = createRnodeService(node)
-    const result = await exploreDeploy({
-      code: checkBalance,
-    })
-
-    return { code: code, result: result }
-  } else {
-    return { code: '', result: '' }
-  }
+  return { code: code, result: result }
 }
 
 export async function handleRequest(event: FetchEvent): Promise<Response> {
   const request = event.request
-  if (request.method === 'POST') {
-    //  const { code, result } = await exploreRequest(request)
+  if (request.method === 'GET') {
+    const { searchParams } = new URL(request.url)
+    const net = searchParams.get('net')
+    const code = searchParams.get('code')
+    if (net && code) {
+      const { result } = await exploreRequest(net, code)
 
-    const hashCode = (s: string) =>
-      s.split('').reduce((a, b) => {
-        a = (a << 5) - a + b.charCodeAt(0)
-        return a & a
-      }, 0)
-    // cache response
-    const concatUrl = request.url + hashCode('code').toString()
-    console.log(concatUrl)
-    const cacheUrl = new URL(concatUrl)
-    //const cacheUrl = new URL('httpcode')
-    const cacheKey = new Request(cacheUrl.toString(), request)
-    const cache = caches.default
+      //console.log(codeParam)
+      const hashCode = (s: string) =>
+        s.split('').reduce((a, b) => {
+          a = (a << 5) - a + b.charCodeAt(0)
+          return a & a
+        }, 0)
+      /* Cache matching */
+      const { hostname } = new URL(request.url)
+      const concatUrl = 'https://' + hostname + '/' + hashCode(code).toString()
 
-    const response = await cache.match(cacheKey)
+      const cacheUrl = new URL(concatUrl)
+      //const cacheUrl = new URL('httpcode')
+      const cacheKey = new Request(cacheUrl.toString(), request)
+      const cache = caches.default
 
-    if (!response) {
-      // Must use Response constructor to inherit all of response's fields
-      const response = new Response(JSON.stringify('result'), { status: 200 })
+      const response = await cache.match(cacheKey)
 
-      // Cache API respects Cache-Control headers. Setting s-max-age to 10
-      // will limit the response to be in cache for 10 seconds max
+      if (!response) {
+        // Must use Response constructor to inherit all of response's fields
+        const response = new Response(JSON.stringify(result), { status: 200 })
 
-      // Any changes made to the response here will be reflected in the cached value
-      response.headers.append('Cache-Control', 's-maxage=31556952')
+        // Cache API respects Cache-Control headers. Setting s-max-age to 10
+        // will limit the response to be in cache for 10 seconds max
 
-      // Store the fetched response as cacheKey
-      // Use waitUntil so you can return the response without blocking on
-      // writing to cache
-      event.waitUntil(cache.put(cacheKey, response.clone()))
+        // Any changes made to the response here will be reflected in the cached value
+        response.headers.append('Cache-Control', 's-maxage=31556952')
+
+        // Store the fetched response as cacheKey
+        // Use waitUntil so you can return the response without blocking on
+        // writing to cache
+        event.waitUntil(cache.put(cacheKey, response.clone()))
+        return response
+      }
       return response
     }
+    const response = new Response('Expected Arguments', { status: 500 })
     return response
   } else {
     const response = new Response('Expected POST', { status: 500 })
